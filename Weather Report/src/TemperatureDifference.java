3da6b6393ff75fd1c8efc01c5e7e204acfac3c60
19khan <17hanj19@millshighsch.org>
2017-02-22 10:46:38 -0800

    ,
    .

---
+++ Weather Report/src/TemperatureDifference.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class TemperatureDifference {
+	public static void main (String args []) throws FileNotFoundException{
+		Scanner input = new Scanner (new File("./resources/weather.txt"));
+		ArrayList <Double> data = new ArrayList<Double>();
+				
+		while (input.hasNext() == true){
+			data.add(input.nextDouble());
+		}	
+		for ()
+	}
+}
